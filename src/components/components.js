8085275a7555a2435d666f1d8f10a2da6365c78e
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-26 14:52:46 +0700

    commit

--- src/components/components.js
+++ src/components/components.js
@@ -3,6 +3,7 @@ import { setTheme } from "./color/color.js";
 import "./component/component.js";
 import "./router/router.js";
 import "./store/store.js";
+
 import "./ripple/ripple.js";
 import "./icon/icon.js";
 import "./button/button.js";
