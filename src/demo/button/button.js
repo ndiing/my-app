d078fca2e9cef47ca569c5e99d29f1ece1b9c104
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-26 14:41:08 +0700

    commit

--- src/demo/button/button.js
+++ src/demo/button/button.js
@@ -6,28 +6,28 @@ class DemoButtonComponent extends MyComponent {
         /* prettier-ignore */
         return html`
             <div class="my-grid">
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4">
-                    <my-button variant="elevated" label="Text Button" ></my-button>
-                    <my-button variant="elevated" label="Text Button" icon="add" ></my-button>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4">
+                    <my-button variant="elevated" label="Elevated button" ></my-button>
+                    <my-button variant="elevated" label="Elevated button" icon="add" ></my-button>
                 </div>
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4">
-                    <my-button variant="filled" label="Text Button" ></my-button>
-                    <my-button variant="filled" label="Text Button" icon="add" ></my-button>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4">
+                    <my-button variant="filled" label="Filled button" ></my-button>
+                    <my-button variant="filled" label="Filled button" icon="add" ></my-button>
                 </div>
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4">
-                    <my-button variant="tonal" label="Text Button" ></my-button>
-                    <my-button variant="tonal" label="Text Button" icon="add" ></my-button>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4">
+                    <my-button variant="tonal" label="Filled tonal button" ></my-button>
+                    <my-button variant="tonal" label="Filled tonal button" icon="add" ></my-button>
                 </div>
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4">
-                    <my-button variant="outlined" label="Text Button" ></my-button>
-                    <my-button variant="outlined" label="Text Button" icon="add" ></my-button>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4">
+                    <my-button variant="outlined" label="Outlined button" ></my-button>
+                    <my-button variant="outlined" label="Outlined button" icon="add" ></my-button>
                 </div>
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4">
-                    <my-button label="Text Button" ></my-button>
-                    <my-button label="Text Button" icon="add" ></my-button>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4">
+                    <my-button label="Text button" ></my-button>
+                    <my-button label="Text button" icon="add" ></my-button>
                 </div>
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4"></div>
-                <div class="my-grid__item--expanded4 my-grid__item--medium4 my-grid__item--compact4"></div>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4"></div>
+                <div class="my-grid__item--expanded12 my-grid__item--medium4 my-grid__item--compact4"></div>
             </div>
         `;
     }
